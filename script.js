e7f9487d20f50269b889e44a4e9673d49c6fce7b
Eric D. Torres <etorresnotary@gmail.com>
2020-11-16 13:42:42 -0600

    modularizing the color input selectors

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
 
-color1.addEventListener("input", () => {
+function colorPicker() {
   body.style.background =
     "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-});
+}
 
-color2.addEventListener("input", () => {
-  body.style.background =
-    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-});
+color1.addEventListener("input", colorPicker);
+
+color2.addEventListener("input", colorPicker);
