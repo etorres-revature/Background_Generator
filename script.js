af9428790998b355f275c3b050fec44c44d5e5d7
Eric D. Torres <etorresnotary@gmail.com>
2020-11-16 13:39:30 -0600

    gradient inputs changing

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
-
-console.log("CSS:", css);
-console.log("color1:", color1);
-console.log("color2:", color2);
+const body = document.getElementById("gradient");
 
 color1.addEventListener("input", () => {
-  console.log("color one input", color1.value);
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 });
 
 color2.addEventListener("input", () => {
-  console.log("color two input", color2.value);
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 });
